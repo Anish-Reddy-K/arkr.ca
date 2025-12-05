09b7bdabe41b17264a154f6b36f463a39f64d2e8
Anish Reddy <akanamathareddy5037@conestogac.on.ca>
2025-12-05 01:09:55 -0500

    update .gitignore

--- api/spotify_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// spotify API credentials
-return [
-    'clientId' => '4d927aa81ba544029d7d189f85afc027', 
-    'clientSecret' => 'cc6f3fc9765b4fe685559136cc8657bb',
-    'refreshToken' => 'AQBaITNnz6WprxTzXDaAolid5_Y3hTaKxtyAu5Jz5XPfryD0QJyABoBBno3wT8lZ86N_IUmWMb9DX_AQRCzRWeSLi0THxqixBdfObwomjuvhI6kb7reBKsBL3oarjPJcldE'
-];
-?>
